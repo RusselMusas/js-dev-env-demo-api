dffc132f9fcde6ccf815cde10a258858d7342fd1
Russel Musasa <russel.musasa@gmail.com>
2021-05-01 07:27:55 +0100

    Updated on 01-05-2021

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Russel","lastName":"Musas","email":"rmusas@yahoo.com"},
+    {"id": 2,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 3,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 4,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
